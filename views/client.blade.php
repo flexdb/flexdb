835e69b5f1451bdb51286bf12cafc0368434c415
huxain <h.xain@yahoo.com>
2017-02-12 01:55:39 +0700

    added production version to client.blade if env prod is set

--- views/client.blade.php
+++ views/client.blade.php
@@ -3,9 +3,13 @@
 @include('fwb::_head')
 
 <body class="theme-default colorful-enabled" ng-app="app">
+    @if(env('APP_ENV') == 'prod')
+    <link rel="stylesheet" href="/js/common.bundle.css" />
+    <link rel="stylesheet" href="/js/app.css" />
+    @else
     <link rel="stylesheet" href="http://{{env('NPM_SERVER', 'localhost')}}:8088/common.bundle.css" />
     <link rel="stylesheet" href="http://{{env('NPM_SERVER', 'localhost')}}:8088/app.css" />
-    
+    @endif
     <nav ui-view="leftnav" class="left-menu" left-menu ng-class="{'hidden-left-menu': hideLeftMenu}">
         <!-- content inserted from angular ui-view -->
     </nav>
@@ -19,10 +23,15 @@
 
 <div class="main-backdrop"><!-- --></div>
  
-            
+    @if(env('APP_ENV') == 'prod')
+    <script src="/js/common.bundle.js"></script>
+    <script src="/js/app.js"></script>
+    @else
+    <script src="//{{env('NPM_SERVER', 'localhost')}}:8088/common.bundle.js"></script>
+    <script src="http://{{env('NPM_SERVER', 'localhost')}}:8088/app.js"></script>
         
-        <script src="//{{env('NPM_SERVER', 'localhost')}}:8088/common.bundle.js"></script>
-        <script src="http://{{env('NPM_SERVER', 'localhost')}}:8088/app.js"></script>
+    @endif
+        
         <!--<script src="/js/helpers.js"></script>-->
         
     
